b7d0bd989301676767115abc79c0bd2754ef98a3
Ho yeon KIM <hoyeonkim@Hos-MacBook-Pro.local>
2020-09-08 18:39:22 -0700

    express server lies under graphql, no schema yet

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("work!")
+//modules first then lib comes later in alphabetical order in typescript.
+import { Options } from "graphql-yoga";
+import app from "./app";
+
+const PORT: number | string = process.env.PORT || 4000;
+const PLAYGROUND: string = "/playground";
+const GRAPHQL_ENDPOINT: string = "/grpahql";
+
+const appOptions: Options = {
+  port: PORT,
+  playground: PLAYGROUND,
+  endpoint: GRAPHQL_ENDPOINT,
+};
+
+const handleAppStart = () => console.log(`Listening on port ${PORT}`);
+
+
+app.start(appOptions, handleAppStart);
