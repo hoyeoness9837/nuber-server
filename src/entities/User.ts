69ae9c14f33f51a621394cbbc609bd9a87a8fdd7
Ho yeon KIM <hoyeonkim@Hos-MacBook-Pro.local>
2020-09-11 22:33:48 -0700

    Just made password, phonenumber and profilephoto nullable for testing purpose.

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -39,16 +39,16 @@ class User extends BaseEntity {
   @Column({ type: "int", nullable: true })
   age: number;
 
-  @Column({ type: "text" })
+  @Column({ type: "text", nullable: true })
   password: string;
 
-  @Column({ type: "text" })
+  @Column({ type: "text", nullable: true })
   phoneNumber: string;
 
   @Column({ type: "boolean", default: false })
   verifiedPhoneNumber: boolean;
 
-  @Column({ type: "text" })
+  @Column({ type: "text", nullable: true })
   profilePhoto: string;
 
   @Column({ type: "boolean", default: false })
