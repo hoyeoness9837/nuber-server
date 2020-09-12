2f2e34df4a6b8a12f3e3e6a1effda89bbbbff7e5
Ho yeon KIM <hoyeonkim@Hos-MacBook-Pro.local>
2020-09-11 19:57:41 -0700

    rider, driver, verification relationships to user data are created.

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -8,12 +8,14 @@ import {
   CreateDateColumn,
   Entity,
   ManyToOne,
+  OneToMany,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
-  OneToMany,
 } from "typeorm";
 import Chat from "./Chat";
 import Message from "./Message";
+import Ride from "./Ride";
+import Verification from "./Verification";
 
 const BCRYPT_ROUNDS = 10;
 
@@ -72,6 +74,15 @@ class User extends BaseEntity {
 
   @OneToMany((type) => Message, (message) => message.user)
   messages: Message[];
+
+  @OneToMany((type) => Verification, (verfication) => verfication.user)
+  verfication: Verification[];
+
+  @OneToMany((type) => Ride, (ride) => ride.passenger)
+  ridesAsPassenger: Ride[];
+
+  @OneToMany((type) => Ride, (ride) => ride.driver)
+  ridesAsDriver: Ride[];
 
   @CreateDateColumn()
   createdAt: string;
--- src/entities/Verification.ts
+++ src/entities/Verification.ts
@@ -4,10 +4,12 @@ import {
   Column,
   CreateDateColumn,
   Entity,
+  ManyToOne,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from "typeorm";
 import { verificationTarget } from "src/types/types";
+import User from "./User";
 
 const PHONE = "PHONE";
 const EMAIL = "EMAIL";
@@ -28,7 +30,11 @@ class Verification extends BaseEntity {
   @Column({ type: "boolean", default: false })
   used: boolean;
 
-  @CreateDateColumn() createdAt: string;
+  @ManyToOne((type) => User, (user) => user.verfication)
+  user: User;
+
+  @CreateDateColumn()
+  createdAt: string;
 
   @UpdateDateColumn() updatedAt: string;
 
