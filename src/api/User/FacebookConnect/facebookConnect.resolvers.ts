28a518388b4b7f613551893d46aa33ad3d662bf4
Ho yeon KIM <hoyeonkim@Hos-MacBook-Pro.local>
2020-09-11 21:26:29 -0700

    error fixed: must provide schema definition with query

---
+++ src/api/User/FacebookConnect/facebookConnect.resolvers.ts
@@ -0,0 +1,15 @@
+// import { } from "src/types/graph";
+// import { Resolvers } from "src/types/resolvers";
+
+// const resolvers: Resolvers = {
+//   Mutation: {
+//     FacebookConnect: async (
+//       _,
+//       args: FacebookConnectMutationArgs
+//     ): Promise<FacebookConnectResponse> => {
+//       ("");
+//     },
+//   },
+// };
+
+// export default resolvers;
