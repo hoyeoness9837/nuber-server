ba0c26eea56f42c768b4863dd9531fb4ac2099fb
Ho yeon KIM <hoyeonkim@Hos-MacBook-Pro.local>
2020-09-12 18:01:05 -0700

    to test SMS, commented out some code. Used type is removed.

--- src/entities/Verification.ts
+++ src/entities/Verification.ts
@@ -27,9 +27,6 @@ class Verification extends BaseEntity {
   @Column({ type: "text" })
   key: string;
 
-  // @Column({ type: "boolean", default: false })
-  // used: boolean;
-
   // @ManyToOne((type) => User, (user) => user.verification)
   // user: User;
 
