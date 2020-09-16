38b45b20f7e8d01448a2144cfe006b3bd114d4ab
Ho yeon KIM <hoyeonkim@Hos-MacBook-Pro.local>
2020-09-16 14:27:03 -0700

    nearby side subscription is updated.

--- src/api/Ride/RequestRide/ReuqestRide.resolvers.ts
+++ src/api/Ride/RequestRide/ReuqestRide.resolvers.ts
@@ -13,21 +13,32 @@ const resolvers: Resolvers = {
       async (
         _,
         args: RequestRideMutationArgs,
-        { req }
+        { req, pubSub }
       ): Promise<RequestRideResponse> => {
         const user: User = req.user;
-        try {
-          const ride = await Ride.create({ ...args, passenger: user }).save();
-          return {
-            ok: true,
-            error: null,
-            ride,
-          };
-        } catch (error) {
-          return {
-            ok: false,
-            error: error.message,
-            ride: null,
+        if   (!user.isRiding) {
+          try {
+            const ride = await Ride.create({ ...args, passenger: user }).save();
+            pubSub.publish("rideRequest", {    NearbyRideSubscription: ride    });
+            user.isRiding = true,
+            user.save()
+            return {
+              ok: true,
+              error: null,
+              ride,
+            };
+          } catch (error) {
+            return {
+              ok: false,
+              error: error.message,
+              ride: null,
+            };
+          }
+        } else {
+          return   {
+            ok:   false,
+            error: "You cannot request two rides",
+            ride:   null,
           };
         }
       }
