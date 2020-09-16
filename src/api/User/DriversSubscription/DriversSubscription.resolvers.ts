61f4e4316aaf6c15c2e5fe98166d685396120077
Ho yeon KIM <hoyeonkim@Hos-MacBook-Pro.local>
2020-09-15 23:57:32 -0700

    filtering subscription message are updated.

--- src/api/User/DriversSubscription/DriversSubscription.resolvers.ts
+++ src/api/User/DriversSubscription/DriversSubscription.resolvers.ts
@@ -1,9 +1,29 @@
+import { withFilter } from "graphql-yoga";
+import User from "../../../entities/User";
+
+// filter driver object that carries payload that is going be returned to users who are at the lastlat and lastlng is close driverslastlat and lng.
 const resolvers = {
   Subscription: {
     DriversSubscription: {
-      subscribe: (_, __, { pubSub }) => {
-        return pubSub.asyncIterator("driverUpdate");
-      },
+      subscribe: withFilter(
+        (_, __, { pubSub }) => pubSub.asyncIterator("driverUpdate"),
+        (payload, _, { context }) => {
+          const user: User = context.currentUser;
+          const {
+            DriversSubscription: {
+              lastLat: driverLastLat,
+              lastLng: driverLastLng,
+            },
+          } = payload;
+          const { lastLat: userLastLat, lastLng: userLastLng } = user;
+          return (
+            driverLastLat >= userLastLat - 0.05 &&
+            driverLastLat <= userLastLat + 0.05 &&
+            driverLastLng >= userLastLng - 0.05 &&
+            driverLastLng <= userLastLng + 0.05
+          );
+        }
+      ),
     },
   },
 };
